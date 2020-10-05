2ba7490faf8dab4ea17dfa49f200ed1f9e58e81f
Lindsey Gobert <lmgobert1@gmail.com>
2020-10-05 08:43:49 -0500

    Updates

---
+++ precache-manifest.55a304aefa24aab16914063c46cfabb1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "234899ea15e8f562b41c77044a0f7815",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "fc810065d2f27605388e",
+    "url": "/React-Portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "d341a4dd97e97ea2e7a7",
+    "url": "/React-Portfolio/static/css/main.cab17ab1.chunk.css"
+  },
+  {
+    "revision": "fc810065d2f27605388e",
+    "url": "/React-Portfolio/static/js/2.0752a909.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React-Portfolio/static/js/2.0752a909.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d341a4dd97e97ea2e7a7",
+    "url": "/React-Portfolio/static/js/main.17dbb5af.chunk.js"
+  },
+  {
+    "revision": "56993ab3d4c34264a97e",
+    "url": "/React-Portfolio/static/js/runtime-main.a571859c.js"
+  },
+  {
+    "revision": "828c10776e11ef6255c7531647123139",
+    "url": "/React-Portfolio/static/media/BirdieHomepage.828c1077.png"
+  },
+  {
+    "revision": "599f1c20cd822a6026173ad7dc610457",
+    "url": "/React-Portfolio/static/media/Epicure.599f1c20.png"
+  },
+  {
+    "revision": "fdfc7a2c5bcd5dbeddc3305eaa35162e",
+    "url": "/React-Portfolio/static/media/PasswordGenerator2.fdfc7a2c.png"
+  }
+]);
